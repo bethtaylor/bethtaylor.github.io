9119756a90d7eafab35a7d9e9c6129ff8ee4ab65
Beth Taylor <beth@nntaylor.net>
2015-11-29 14:17:45 -0800

    recipe data

---
+++ recipe/recipedata.js
@@ -0,0 +1,126 @@
+var recipes = {
+	chocolate_cake: {
+		title: "Chocolate Cake",
+		ingredients: [
+			"flour",
+			"milk",
+			"chocolate"
+		],
+		directions: [
+			"preheat oven to 350",
+			"bake for 30 minutes",
+		],
+		image: "http://www.chowstatic.com/blog-media/2015/01/1.jpg",
+	},
+	waffle: {
+		title: "Waffle Recipe",
+		ingredients: [
+			"flour",
+			"milk",
+			"chocolate"
+		],
+		directions: [
+			"preheat oven to 350",
+			"bake for 30 minutes",
+		],
+		image: "http://www.twopeasandtheirpod.com/wp-content/uploads/2013/09/Brown-Butter-Pumpkin-Waffles-4.jpg",
+	},
+	bread: {
+		title: "Bread Recipe",
+		ingredients: [
+			"flour",
+			"milk",
+			"chocolate"
+		],
+		directions: [
+			"preheat oven to 350",
+			"bake for 30 minutes",
+		],
+		image: "https://upload.wikimedia.org/wikipedia/commons/7/71/Anadama_bread_(1).jpg",
+	},
+	sushi: {
+		title: "Shushi Recipe",
+		ingredients: [
+			"flour",
+			"milk",
+			"chocolate"
+		],
+		directions: [
+			"preheat oven to 350",
+			"bake for 30 minutes",
+		],
+		image: "http://www.house-sushi.com/wp-content/uploads/2013/12/sushi.jpg",
+	},
+	pizza: {
+		title: "Pizza Recipe",
+		ingredients: [
+			"flour",
+			"milk",
+			"chocolate"
+		],
+		directions: [
+			"preheat oven to 350",
+			"bake for 30 minutes",
+		],
+		image: "https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg",
+	},
+	salad: {
+		title: "Salad Recipe",
+		ingredients: [
+			"flour",
+			"milk",
+			"chocolate"
+		],
+		directions: [
+			"preheat oven to 350",
+			"bake for 30 minutes",
+		],
+		image: "http://www.cornerbakerycafe.com/getmedia/63a151e3-77d3-4fd4-9d86-624f7616e9c0/Chopped-Salad_550x310?width=550&height=310&ext=.jpg",
+	},
+	pasta: {
+		title: "Pasta Recipe",
+		ingredients: [
+			"flour",
+			"milk",
+			"chocolate"
+		],
+		directions: [
+			"preheat oven to 350",
+			"bake for 30 minutes",
+		],
+		image: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Asian_Style_Italian_Pasta.jpg",
+	},
+	jam: {
+		title: "Jam Recipe",
+		ingredients: [
+			"flour",
+			"milk",
+			"chocolate",
+		],
+		directions: [
+			"preheat oven to 350",
+			"bake for 30 minutes",
+		],
+		image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTTrT0STGzHVV798xljoRlVTzcZPqg1jiAk96BCt-H1JNlg1hSY",
+	},
+	pizza_dough: {
+		title: "Pizza Dough Recipe",
+		ingredients: [
+			"1c hot water",        
+			"1 tbl yeast",
+			"2 tsp sugar",
+			"1 tsp salt",
+			"2 tbl olive oil",
+			"2.5 c flour",
+		],
+		directions: [
+			"Mix yeast, water, and sugar. Let stand.",
+			"In mixing bowl, mix flour, oil, and salt.",
+			"Once yeast mixture has risen, mix all ingredients.",
+			"Let dough rest in bowl covered with a towel in a warm place for 30 minutes.",
+			"oll out dough and add sauce and toppings. Bake at 400 F  for 20 minutes."
+		],
+		image: "http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2011/7/26/2/GT0303_pizza-dough_s4x3.jpg",
+	},
+
+};
